222831c78b934d5fcf77018023efe067f3393eda
Nicolás Soto <sotopereznico@gmail.com>
2026-09-10 14:49:53 -0300

    Commit inicial: Esqueleto del proyecto SpringEduManager generado

---
+++ src/main/java/cl/unitec/springedumanager/SpringedumanagerApplication.java
@@ -0,0 +1,13 @@
+package cl.unitec.springedumanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringedumanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringedumanagerApplication.class, args);
+	}
+
+}
